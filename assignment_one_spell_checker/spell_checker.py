18ecfb48b4a0adabe2f218d191c6f971ac710bb7
Alexander McNeill <mcnear1@student.op.ac.nz>
2015-09-26 23:03:50 +1200

    Starting to work on how i will be recommending corrections

--- assignment_one_spell_checker/spell_checker.py
+++ assignment_one_spell_checker/spell_checker.py
@@ -2,18 +2,24 @@ __author__ = 'alexmcneill'
 
 
 class SpellChecker:
+    """Class that gives you spelling recommendations based on a given string"""
 
     WORDS_FILE = "words"
 
     def __init__(self):
         self.words = self.create_words_set(self.WORDS_FILE)
+        self.common_mistakes = {}
 
     def create_words_set(self, file_location):
-        """Create words set from given file"""
+        """Create words set from given file
+        :rtype : set
+        """
         words_file = open(file_location, 'r')
         words = set()
 
+        #  Reading through the first line of the file to start the reading
         current_word = words_file.readline()
+
         while current_word != "":
             words.add(current_word.rstrip())
             current_word = words_file.readline()
@@ -21,21 +27,73 @@ class SpellChecker:
         return words
 
     def check(self, input_string):
-        """perform a spell check on the input string with respect the words set"""
+        """Method performs a spell check on the input string with respect the words set and returns a list of possible
+        corrections
+        :rtype : bool
+        """
         if input_string in self.words:
             return [input_string]
+        elif input_string in self.common_mistakes:
+            return self.common_mistakes[input_string].corrections
         else:
-            return self.recommend_correct_spelling(input_string)
+            corrections = self.recommend_correct_spelling(input_string)
+            self.cashe_mistake(input_string, corrections)
+            return corrections
 
     def recommend_correct_spelling(self, input_string):
-        """Recommend all possible corrections in the words set"""
-        """Must be able to handle common spelling mistakes like:
-        -swapping adjacent characters
-        -inserting a single character
-        -deleting a single character
-        -replacing a single character
-        -(Extra challenge: consider phonetic substitutions as well)"""
-        pass
+        """Recommend all possible corrections to the input string that are in the words set
+        :rtype : set
+        """
+
+        # Creating a set to contain the possible corrections for the input string
+        corrections = set()
+
+        # Checking each word to see if it is a possible correction to the input string
+        for word in self.words:
+            if self.is_possible_correction(input_string, word):
+                corrections.add(word)
+
+        return corrections
+
+    def cashe_mistake(self, input_string, corrections):
+        """Method that cashes the mistake. Maybe used to retain order of the dict later"""
+        self.common_mistakes[input_string] = corrections
+
+    def is_possible_correction(self, incorrect_word, word):
+        """Method that checks if the input word could be a correction for the input incorrect word
+        :rtype : bool
+        """
+        swapped_check_result = self.check_swapped_characters(incorrect_word, word)
+        missing_check_result = self.check_missing_character(incorrect_word, word)
+        incorrect_check_result = self.check_incorrect_character(incorrect_word, word)
+        phonetic_check_result = self.check_phonetic_substitution(incorrect_word, word)
+
+        #  Returning true if any of the checks were correct
+        return swapped_check_result or missing_check_result or incorrect_check_result or phonetic_check_result
+
+    def check_swapped_characters(self, incorrect_word, word):
+        """Method returns true if the incorrect string could be the word in a different order
+        :rtype : bool
+        """
+        return len(incorrect_word) == len(word) and sorted(incorrect_word) in sorted(word)
+
+    def check_missing_character(self, incorrect_word, word):
+        """Method returns true if the incorrect word could be the word with a missing character
+        :rtype : bool
+        """
+        return False
+
+    def check_incorrect_character(self, incorrect_word, word):
+        """Method returns true if the incorrect word could be the word with one incorrect character
+        :rtype : bool
+        """
+        return False
+
+    def check_phonetic_substitution(self, incorrect_word, word):
+        """Method returns true if the incorrect word could be the word with a incorrect phonetic substitution
+        :rtype : bool
+        """
+        return False
 
 
 class CashedMistake:
